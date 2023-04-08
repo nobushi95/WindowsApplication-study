6618a4af870db5314d7de9d04df87f503701e23f
nobushi95 <nobushi1121@gmail.com>
2023-04-09 00:52:14 +0900

    SharedプロジェクトにNativeMethodsを追加

---
+++ WindowVisibilityAndEventWaitHandle/Shared/NativeMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Shared
+{
+    internal class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern uint WaitForMultipleObjects(uint nCount, IntPtr[] lpHandles,
+   bool bWaitAll, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenEvent(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+        [DllImport("kernel32.dll")]
+        public static extern bool SetEvent(IntPtr hEvent);
+        [DllImport("kernel32.dll")]
+        internal static extern bool ResetEvent(IntPtr hEvent);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+    }
+}
