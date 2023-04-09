1029baa9b3adb663375353d11db3f8f33829d8bd
nobushi95 <nobushi1121@gmail.com>
2023-04-09 15:01:39 +0900

    EventSetterWPFNative実装

--- WindowVisibilityAndEventWaitHandle/EventSetterWPFNative/MainWindow.xaml.cs
+++ WindowVisibilityAndEventWaitHandle/EventSetterWPFNative/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shared;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EventSetterWPFNative
 {
@@ -23,6 +11,20 @@ namespace EventSetterWPFNative
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            var handle = NativeMethods.OpenEvent(NativeMethods.EVENT_MODIFY_STATE, false, Constatns.ShowEventName);
+            NativeMethods.SetEvent(handle);
+            NativeMethods.CloseHandle(handle);
+        }
+
+        private void HideButton_Click(object sender, RoutedEventArgs e)
+        {
+            var handle = NativeMethods.OpenEvent(NativeMethods.EVENT_MODIFY_STATE, false, Constatns.HideEventName);
+            NativeMethods.SetEvent(handle);
+            NativeMethods.CloseHandle(handle);
         }
     }
 }
