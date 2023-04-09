e44083c2d1910f4e3ad221e33bb2412354163baf
nobushi95 <nobushi1121@gmail.com>
2023-04-09 16:13:26 +0900

    EventWaiterWPFNativeのViewModel実装

--- WindowVisibilityAndEventWaitHandle/EventWaiterWPFNative/ViewModels/MainWindowViewModel.cs
+++ WindowVisibilityAndEventWaitHandle/EventWaiterWPFNative/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Shared;
 
 namespace EventWaiterWPFNative.ViewModels
 {
@@ -8,6 +9,78 @@ namespace EventWaiterWPFNative.ViewModels
         private string _receivedEventName = "Nothing";
 
         [ObservableProperty]
-        private bool _isWindowVisible;
+        private bool _isWindowVisible = true;
+
+        public MainWindowViewModel()
+        {
+            // 投げっぱなしタスク
+            Task.Run(EventWait);
+        }
+
+        private void EventWait()
+        {
+            var events = new IntPtr[]
+            {
+                NativeMethods.CreateEvent(IntPtr.Zero, true, false, EventNames.Show),
+                NativeMethods.CreateEvent(IntPtr.Zero, true, false, EventNames.Hide),
+                NativeMethods.CreateEvent(IntPtr.Zero, true, false, EventNames.Close),
+            };
+
+            if (events.Any(x => x == IntPtr.Zero))
+            {
+                CloseEventHandles();
+                return;
+            }
+
+
+            var isLoop = true;
+            while (isLoop)
+            {
+                var result = NativeMethods.WaitForMultipleObjects((uint)events.Length, events, false, NativeMethods.INFINITE);
+
+                switch (result)
+                {
+                    // Showイベント
+                    case NativeMethods.WAIT_OBJECT_0:
+                        ReceivedEventName = "Show";
+                        // Windowを非アクティブにしてCollapsed->Showにすると非アクティブのままだが、
+                        // もう一度非アクティブのままCoolaped->Showにするとアクティブになる？
+                        IsWindowVisible = true;
+                        NativeMethods.ResetEvent(events[0]);
+                        break;
+
+                    // Hideイベント
+                    case NativeMethods.WAIT_OBJECT_0 + 1:
+                        ReceivedEventName = "Hide";
+                        IsWindowVisible = false;
+                        NativeMethods.ResetEvent(events[1]);
+                        break;
+
+                    // Closeイベント
+                    case NativeMethods.WAIT_OBJECT_0 + 2:
+                        ReceivedEventName = "Close";
+                        isLoop = false;
+                        NativeMethods.ResetEvent(events[2]);
+                        // TODO: WindowをCloseする(ループ抜けた後かも検討する)
+                        break;
+
+                    case NativeMethods.WAIT_FAILED:
+                        isLoop = false;
+                        break;
+                }
+            }
+
+            CloseEventHandles();
+
+            void CloseEventHandles()
+            {
+                foreach (var eventHandle in events)
+                {
+                    if (eventHandle == IntPtr.Zero)
+                        continue;
+                    NativeMethods.CloseHandle(eventHandle);
+                }
+            }
+        }
     }
 }
