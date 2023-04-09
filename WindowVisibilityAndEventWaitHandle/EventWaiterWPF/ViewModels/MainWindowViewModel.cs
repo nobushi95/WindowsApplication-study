829dff6ac9a7c5055c11c603845f892aa3a2be95
nobushi95 <nobushi1121@gmail.com>
2023-04-09 22:55:11 +0900

    MainWindowViewModel実装

--- WindowVisibilityAndEventWaitHandle/EventWaiterWPF/ViewModels/MainWindowViewModel.cs
+++ WindowVisibilityAndEventWaitHandle/EventWaiterWPF/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Shared;
 
 namespace EventWaiterWPF.ViewModels
 {
@@ -17,7 +18,58 @@ namespace EventWaiterWPF.ViewModels
 
         private void EventWait()
         {
+            var created = Enumerable.Repeat(true, 3).ToArray();
+            var events = new[]
+            {
+                new EventWaitHandle(false, EventResetMode.ManualReset, EventNames.Show, out created[0]),
+                new EventWaitHandle(false, EventResetMode.ManualReset, EventNames.Hide, out created[1]),
+                new EventWaitHandle(false, EventResetMode.ManualReset, EventNames.Close, out created[2]),
+            };
 
+            if (created.Any(x => !x))
+                Array.ForEach(events, x => x.Dispose());
+
+            var isLoop = true;
+            while (isLoop)
+            {
+                try
+                {
+                    var signaledIdx = WaitHandle.WaitAny(events);
+                    switch (signaledIdx)
+                    {
+                        // Showイベント
+                        case 0:
+                            ReceivedEventName = "Show";
+                            IsWindowVisible = true;
+                            events[0].Reset();
+                            break;
+
+                        // Hideイベント
+                        case 1:
+                            ReceivedEventName = "Hide";
+                            IsWindowVisible = false;
+                            events[1].Reset();
+                            break;
+
+                        // Closeイベント
+                        case 2:
+                            ReceivedEventName = "Close";
+                            isLoop = false;
+                            events[2].Reset();
+                            break;
+
+                        default:
+                            isLoop = false;
+                            break;
+                    }
+                }
+                catch
+                {
+                    isLoop = false;
+                }
+            }
+
+            Array.ForEach(events, x => x.Dispose());
         }
     }
 }
