7adf48a278b84e3598da73fbe1d3bfdd76c2f7d3
nobushi95 <nobushi1121@gmail.com>
2023-04-09 15:28:34 +0900

    ViewModelのプロパティをバインディングするところまで実装

---
+++ WindowVisibilityAndEventWaitHandle/EventWaiterWPFNative/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EventWaiterWPFNative.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _receivedEventName = "Nothing";
+
+        [ObservableProperty]
+        private bool _isWindowVisible;
+    }
+}
