16b2814166de1c25fee1305c66cbaa0f2aba212d
nobushi95 <nobushi1121@gmail.com>
2023-04-09 15:01:26 +0900

    NativeMethods更新

--- WindowVisibilityAndEventWaitHandle/Shared/NativeMethods.cs
+++ WindowVisibilityAndEventWaitHandle/Shared/NativeMethods.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Runtime.ConstrainedExecution;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
 
 namespace Shared
 {
@@ -14,15 +11,19 @@ namespace Shared
 
         [DllImport("kernel32.dll")]
         public static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+
         [DllImport("Kernel32.dll", SetLastError = true)]
         public static extern IntPtr OpenEvent(uint dwDesiredAccess, bool bInheritHandle, string lpName);
+
+        public static readonly uint EVENT_MODIFY_STATE = 0x0002;
+
         [DllImport("kernel32.dll")]
         public static extern bool SetEvent(IntPtr hEvent);
+
         [DllImport("kernel32.dll")]
         internal static extern bool ResetEvent(IntPtr hEvent);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         [SuppressUnmanagedCodeSecurity]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CloseHandle(IntPtr hObject);
