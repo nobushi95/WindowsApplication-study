d7283b822df89b319b79febf500993774ff00d34
nobushi95 <nobushi1121@gmail.com>
2023-04-30 00:39:05 +0900

    CleanAll.bat更新

--- CleanUpAll.bat
+++ CleanUpAll.bat
@@ -5,7 +5,13 @@ for /R %%d in (obj) do ( if exist "%%d" (rmdir /S /Q "%%d" ) )
 echo delete bin
 for /R %%d in (bin) do ( if exist "%%d" (rmdir /S /Q "%%d" ) )
 
-echo delete .vs
-for /R %%d in (.vs) do ( if exist "%%d" ( rmdir /S /Q "%%d" ) )
+echo delete Debug
+for /R %%d in (Debug) do ( if exist "%%d" (rmdir /S /Q "%%d" ) )
+
+echo delete Release
+for /R %%d in (Release) do ( if exist "%%d" (rmdir /S /Q "%%d" ) )
+
+@REM echo delete .vs
+@REM for /R %%d in (.vs) do ( if exist "%%d" ( rmdir /S /Q "%%d" ) )
 
 pause
