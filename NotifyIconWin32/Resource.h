d1ce23059fc06752d8ec9274c8e3403fd664ec51
nobushi95 <nobushi1121@gmail.com>
2023-04-30 15:36:22 +0900

    タスクトレイのメニューをリソースに追加

--- NotifyIconWin32/Resource.h
+++ NotifyIconWin32/Resource.h
@@ -1,30 +1,33 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ で生成されたインクルード ファイルです。
-// 次で使用: NotifyIconWin32.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_NOTIFYICONWIN32_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_NOTIFYICONWIN32			107
-#define IDI_SMALL				108
-#define IDC_NOTIFYICONWIN32			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// 新しいオブジェクトの次の既定値
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// NotifyIconWin32.rc で使用
 //
+#define IDC_MYICON                      2
+#define IDD_NOTIFYICONWIN32_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_NOTIFYICONWIN32             107
+#define IDI_SMALL                       108
+#define IDC_NOTIFYICONWIN32             109
+#define IDR_MAINFRAME                   128
+#define IDR_TRAY_MENU                   129
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_TRAY_OUT                     32773
+#define ID_EXIT                         32774
+#define ID_TRAY_EXIT                    32775
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
