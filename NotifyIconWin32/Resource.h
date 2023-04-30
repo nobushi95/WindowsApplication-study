71d174a25cb1bdce70d871bb9469dd0eab1d44b1
nobushi95 <nobushi1121@gmail.com>
2023-04-30 16:13:02 +0900

    タスクトレイに表示するメニューを追加

--- NotifyIconWin32/Resource.h
+++ NotifyIconWin32/Resource.h
@@ -18,6 +18,8 @@
 #define ID_TRAY_OUT                     32773
 #define ID_EXIT                         32774
 #define ID_TRAY_EXIT                    32775
+#define ID_32776                        32776
+#define ID_TRAY_IN                      32777
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -26,7 +28,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_COMMAND_VALUE         32778
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
